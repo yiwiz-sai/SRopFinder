858f63ef0e4c80bdc3d3d8bfa21ae3871fc77572
SAI <pianoboysai@gmail.com>
2013-03-18 14:23:11 +0800

    SAI:fix some bugs

--- SRopFinder.py
+++ SRopFinder.py
@@ -5,7 +5,6 @@
 import os,sys,time
 import optparse, re
 import distorm3
-import pefile
 
 g_srop_linenumber=0
 g_srop_discernfunc=None
@@ -13,22 +12,27 @@ g_regular_expression=None
 g_hex=None
 
 class InstructionNode(object):
-    def __init__(self, description):
-        self.description=description
+    def __init__(self, va, cur_offset, hexdump, instruction):
+        self.cur_offset=cur_offset
+        self.va=va
+        self.hexdump=hexdump
+        self.instruction=instruction
         self.child=None
         self.parents=[]
         pass
     def __repr__(self):
-        return "%X:\t%-16s\t%s" % self.description
+        return "%X:\t%-16s\t%s" % (self.va, self.hexdump, self.instruction)
     
 def SROP_discern_instruction(instruction, hex):
     global g_regular_expression
+
     if g_regular_expression.match(instruction):
         return True
     return False
 
 def SROP_discern_hex(instruction,  hex):
     global g_hex
+
     if hex==g_hex:
         return True
     return False
@@ -38,7 +42,6 @@ def SROP_discern_hex(instruction,  hex):
 def SROP_findinstruction_at_va(baseaddr, code_start, code_size):
     global g_srop_linenumber
     
-
     instruction_node_map={}
     rop_node_map={}
     
@@ -56,7 +59,7 @@ def SROP_findinstruction_at_va(baseaddr, code_start, code_size):
                 cur_offset=va-baseaddr
                 node=instruction_node_map.get(cur_offset)
                 if not node:
-                    node=InstructionNode((va, hexdump, instruction))
+                    node=InstructionNode(va, cur_offset, hexdump, instruction)
                 else:
                     node_inserted=1
                     
@@ -69,7 +72,7 @@ def SROP_findinstruction_at_va(baseaddr, code_start, code_size):
                     break
                 
                 if g_srop_discernfunc(instruction,hexdump):
-                    rop_node_map[cur_offset]=node
+                    rop_node_map[cur_offset]=node#whct instruction we need is contained in this node
                 
                 instruction_node_map[cur_offset]=node
         except:
@@ -79,31 +82,29 @@ def SROP_findinstruction_at_va(baseaddr, code_start, code_size):
     
 def SROP_show_result(rop_node_map):
     def show_rop_node_chain(rop_node_map, n, node, node_chain):
-        if n:
-            origin_len=len(node_chain)
-            if node.parents:
-                for i in node.parents:
-                    if i not in rop_node_map:
-                        node_chain=node_chain[:origin_len]
-                        node_chain.append(i)
-                        show_rop_node_chain(rop_node_map,n-1,  i, node_chain)
-            else:
-                #node number of this chain is not less than n
-                for i in node_chain[::-1]:
-                    print i
-                print '='*30
-            
-        else:
+        output=0
+        origin_len=len(node_chain)
+        if n and node.parents:
+            for i in node.parents:
+                if (i.cur_offset not in rop_node_map) and (not i.instruction.startswith('DB ')):
+                    node_chain=node_chain[:origin_len]
+                    node_chain.append(i)
+                    show_rop_node_chain(rop_node_map,n-1,  i, node_chain)
+                    output=1
+        
+        if not output:
             #accessing this chain complete
             for i in node_chain[::-1]:
                 print i
             print '='*30
-        
+    
+
     l=rop_node_map.items()
     l.sort()
     for offset, node in l:
         show_rop_node_chain(rop_node_map, g_srop_linenumber, node, [node])
-        
+    
+
     
 if __name__=='__main__':
     code=open('distorm3.dll', 'rb').read()
@@ -116,8 +117,8 @@ if __name__=='__main__':
     #g_regular_expression=re.compile('CALL.*[ ]EAX')
     #g_regular_expression=re.compile('POP.*[ ]ESI')
     g_srop_discernfunc=SROP_discern_instruction
-    SROP_findinstruction_at_va(0x10000000, code[0x400:], 0xa200)
-    
+    SROP_findinstruction_at_va(0x10001000, code[0x400:], 0xa200)
+    #SROP_findinstruction_at_va(0x10000000, code[0xA100:], 0x200)
     sys.exit(0)
     #===================search hex=====================
     g_hex='C3'
